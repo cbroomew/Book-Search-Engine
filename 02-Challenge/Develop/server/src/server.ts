eb1c8ed53b105c42aaea0c7b53a2461c8e0c1040
cbroomew <chad.webster@yahoo.com>
2025-02-20 13:39:20 -0500

    dist

--- 02-Challenge/Develop/server/src/server.ts
+++ 02-Challenge/Develop/server/src/server.ts
@@ -1,18 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
-import path from 'node:path';
+// import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.static('client/dist'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.use(routes);
 
